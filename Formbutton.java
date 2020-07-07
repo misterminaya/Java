c9fb5699d794585190e2084133578d5b6bfbb6ce
misterminaya <misterminayape@gmail.com>
2020-07-06 22:07:00 -0500

    swing java library - JLabel

---
+++ Formbutton.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Formbutton extends JFrame implements ActionListener{
+  
+  JButton boton1;
+  public Formbutton(){
+   setLayout(null);
+   boton1 = new JButton("Cerrar");
+   boton1.setBounds(300,250,100,30);
+   add(boton1);
+   boton1.addActionListener(this);    
+ }
+   public void actionPerformed(ActionEvent e){
+    if(e.getSource() == boton1){
+      System.exit(0);
+    }
+ }
+   public static void main(String args[]){
+   Formbutton formulario1 = new Formbutton();
+   formulario1.setBounds(0,0,450,350);
+   formulario1.setVisible(true);
+   formulario1.setResizable(false);
+   formulario1.setLocationRelativeTo(null);
+ }
+}
