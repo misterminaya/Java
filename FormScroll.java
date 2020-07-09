c04bb722452febb7d42f09f9c3d83ae8525ef49b
misterminaya <misterminayape@gmail.com>
2020-07-08 22:34:24 -0500

    swing java library  - JScrollPane

---
+++ FormScroll.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class FormScroll extends JFrame{
+
+ private JTextField textfield1;
+ private JScrollPane scrollpane1;
+ private JTextArea textarea1;
+
+ public FormScroll(){
+  setLayout(null);
+  textfield1 = new JTextField();
+  textfield1.setBounds(10,10,200,30);
+  add(textfield1);
+  
+  textarea1 = new JTextArea();
+  scrollpane1 = new JScrollPane(textarea1);
+  scrollpane1.setBounds(10,50,400,300);
+  add(scrollpane1);
+ }
+
+ public static void main(String args[]){
+   FormScroll formulario1 = new FormScroll();
+   formulario1.setBounds(0,0,540,400);
+   formulario1.setVisible(true);
+   formulario1.setResizable(false);
+   formulario1.setLocationRelativeTo(null);
+ }
+}
